4a06bd81c8f6004763e14e0f56dc77fdb4216acc
masaun <masanoriuno@gmail.com>
2020-03-27 22:10:18 +0900

    Add deposit() and holdingAndEarnInterest()

--- solidity/contracts/BancorPoolIntegrateWithLendingProtocol.sol
+++ solidity/contracts/BancorPoolIntegrateWithLendingProtocol.sol
@@ -1,10 +1,27 @@
 pragma solidity 0.4.26;
 
 /**
- * The BancorPoolIntegrateWithLendingProtocol contract does this and that...
+ * The BancorPoolIntegrateWithLendingProtocol contract does this and that integrate bancor pool with lending protocol
  */
 contract BancorPoolIntegrateWithLendingProtocol {
   	constructor() public {
     
   	}
+
+    /**
+     * @dev - deposit (Add Liguidly) with DAI
+     **/
+    function deposit() public returns (bool) {
+        // In progress
+    }
+
+
+    /**
+     * @dev - Holding and earn interest with lending protocol. (e.g. cDAI, yDAI)
+     **/
+    function holdingAndEarnInterest() public returns (bool) {
+        // In progress
+    }
+
+    
 }
