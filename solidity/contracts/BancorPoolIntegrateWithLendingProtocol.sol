9a2351ee0e97962bbaaf0ce26073244992f6e5ef
masaun <masanoriuno@gmail.com>
2020-03-27 12:52:31 +0900

    Add BancorPoolIntegrateWithLendingProtocol.sol

---
+++ solidity/contracts/BancorPoolIntegrateWithLendingProtocol.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.4.26;
+
+/**
+ * The BancorPoolIntegrateWithLendingProtocol contract does this and that...
+ */
+contract BancorPoolIntegrateWithLendingProtocol {
+  	constructor() public {
+    
+  	}
+}
