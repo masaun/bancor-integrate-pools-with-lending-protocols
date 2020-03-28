c1afbaec67f9917afe165c2cc0f370a8a8da10f7
masaun <masanoriuno@gmail.com>
2020-03-28 11:43:32 +0900

    Add parameters to deposit()

--- solidity/contracts/BancorPoolIntegrateWithLendingProtocol.sol
+++ solidity/contracts/BancorPoolIntegrateWithLendingProtocol.sol
@@ -1,18 +1,27 @@
 pragma solidity 0.4.26;
 
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
 /**
  * The BancorPoolIntegrateWithLendingProtocol contract does this and that integrate bancor pool with lending protocol
  */
-contract BancorPoolIntegrateWithLendingProtocol {
-  	constructor() public {
+contract BancorPoolIntegrateWithLendingProtocol is Ownable {
+    using SafeMath for uint;
     
+  	constructor(address _erc20) public {
+        erc20 = IERC20(_erc20);
   	}
 
     /**
      * @dev - deposit (Add Liguidly) with DAI
      **/
-    function deposit() public returns (bool) {
+    function deposit(address _contract, uint _amount) public returns (bool) {
         // In progress
+        erc20.transfer(_contract, _amount);
     }
 
 
