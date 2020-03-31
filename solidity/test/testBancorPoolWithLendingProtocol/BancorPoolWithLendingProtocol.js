0791b2c71615435cd0948ba547e2d9ce06673b11
masaun <masanoriuno@gmail.com>
2020-03-31 11:19:18 +0900

    Add test of 'Testing addReserve cDAI and create cDAIBNT of PoolToken'

--- solidity/test/testBancorPoolWithLendingProtocol/BancorPoolWithLendingProtocol.js
+++ solidity/test/testBancorPoolWithLendingProtocol/BancorPoolWithLendingProtocol.js
@@ -21,6 +21,10 @@ const BancorPoolWithLendingProtocol = artifacts.require('BancorPoolWithLendingPr
 
 
 contract('BancorPoolWithLendingProtocol', accounts => {
+    before(async () => {
+        //@dev - In progress
+    });
+
     it('Testing BancorPoolWithLendingProtocol.sol', async () => {
         //@dev - Define assigned value in constructor of BancorPoolWithLendingProtocol.sol
         const _bancorNetwork = BancorNetwork.address;
@@ -49,8 +53,41 @@ contract('BancorPoolWithLendingProtocol', accounts => {
                                                                                     _erc20,
                                                                                     _cERC20,
                                                                                     _yDAI);
-        // Log for just comfirming
+        // Log just for comfirming
         console.log("=== Finished testing of BancorPoolWithLendingProtocol.sol ===");
+    });
+
+
+    it('Testing addReserve cDAI and create cDAIBNT of PoolToken', async () => {
+        //@dev - Define assigned value in constructor of BancorPoolWithLendingProtocol.sol
+        const _bancorNetwork = BancorNetwork.address;
+        const _bancorConverter = BancorConverter.address;
+        const _bancorConverterFactory = BancorConverterFactory.address;
+        const _bancorConverterUpgrader = BancorConverterUpgrader.address;
+        const _bancorFormula = BancorFormula.address;
+        const _contractRegistry = ContractRegistry.address;
+        const _contractFeatures = ContractFeatures.address;
+        const _testERC20Token = TestERC20Token.address;
+        const _smartToken = SmartToken.address;
+        const _erc20 = "0xaD6D458402F60fD3Bd25163575031ACDce07538D";   // DAI on Ropsten
+        const _cERC20 = "0x6CE27497A64fFFb5517AA4aeE908b1E7EB63B9fF";  // cDAI on Ropsten
+        const _yDAI = "0x9D25057e62939D3408406975aD75Ffe834DA4cDd";
+
+        //@dev - Create instance of BancorPoolWithLendingProtocol contract
+        let bancorPoolWithLendingProtocol = await BancorPoolWithLendingProtocol.new(_bancorNetwork, 
+                                                                                    _bancorConverter, 
+                                                                                    _bancorConverterFactory,
+                                                                                    _bancorConverterUpgrader,
+                                                                                    _bancorFormula,
+                                                                                    _contractRegistry,
+                                                                                    _contractFeatures,
+                                                                                    _testERC20Token,
+                                                                                    _smartToken,
+                                                                                    _erc20,
+                                                                                    _cERC20,
+                                                                                    _yDAI);
+        // Log just for comfirming
+        console.log("=== Finished testing addReserve cDAI and create cDAIBNT of PoolToken ===");
     });
 });
 
