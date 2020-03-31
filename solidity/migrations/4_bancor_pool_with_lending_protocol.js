66a5ac3cc5f626ee283c1b743a04bd6843e60fad
masaun <masanoriuno@gmail.com>
2020-03-31 22:14:20 +0900

    Just copy from migration file of '3_deploy_contracts.js' to '4_bancor_pool_with_lending_protocol.js'

--- solidity/migrations/4_bancor_pool_with_lending_protocol.js
+++ solidity/migrations/4_bancor_pool_with_lending_protocol.js
@@ -12,6 +12,8 @@ const TestERC20Token = artifacts.require('TestERC20Token');
 const BancorConverterFactory = artifacts.require('BancorConverterFactory');
 const BancorConverterUpgrader = artifacts.require('BancorConverterUpgrader');
 
+const Web3Utils = require('web3-utils');
+
 var _bancorNetwork = BancorNetwork.address;
 var _bancorConverter = BancorConverter.address;
 var _bancorConverterFactory = BancorConverterFactory.address;
@@ -29,7 +31,9 @@ var _cERC20 = "0x6CE27497A64fFFb5517AA4aeE908b1E7EB63B9fF";  // cDAI on Ropsten
 var _yDAI = "0x9D25057e62939D3408406975aD75Ffe834DA4cDd";
 
 
-module.exports = async function(deployer) {
+
+
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(BancorPoolWithLendingProtocol, 
                           _bancorNetwork,
                           _bancorConverter,
@@ -46,46 +50,191 @@ module.exports = async function(deployer) {
                           //_cToken, 
                           _yDAI);
 
+    //@dev - Just copy from migration file of '3_deploy_contracts.js' below
+    let overwrite = true;
+    let retry = false;
 
-    let contracts = {};
+    const weight10Percent = 100000;
+    const gasPrice = 22000000000;
+    const gasPriceBadHigh = 22000000001;
 
-    let converter = contracts['CONVERTER'];
+    if (network == "production" || network == "development" || true) {
+        deployer.then( async () => {
+            let reached = 0;
+            let current = 0;
 
-    //@dev - Step #6: Funding & Initial Supply
-    let ir = await smartToken.issue(accounts[0], 20000);
-    // console.log('ir res', ir);
+            function getConversionAmount(transaction, logIndex = 0) {
+                return transaction.logs[logIndex].args._return.toNumber();
+            }
 
-    //@dev - Step #7: Activation
-    let tr = await contracts['CONNECTOR_1'].transfer(converter.address, 5000);
-    // console.log('tr res', tr);
+            const trystage = async (method) => {
+                current += 1;
+                console.log('current stage is ', current);
+                console.log('reached stage is ', reached);
+                if (reached <= current) {
+                    // We haven't passed this stage, so try it
 
-    //@dev - Step #7: Activation
-    let t0r = await smartToken.transferOwnership(converter.address);
-    // console.log('t0r res', t0r);
+                    if (reached < current) {
+                        //    This is the first time we've tried this stage, reset failure counts
+                        failures = 0;
+                    }
 
-    //@dev - Step #7: Activation
-    let a0r = await converter.acceptTokenOwnership();
-    // console.log('a0r res', a0r);
+                    reached = current;
+                    return method
+                }
+            };
 
-    let approveRes = await connectorToken.approve(converter.address, 500000000);
+            const registerContract = async (instance,name) => {
+                contracts[name] = instance;
+                let contractIds = contracts['CONTRACT_IDS'];
+                let contractRegistry = contracts['CONTRACT_REGISTRY'];
 
-    console.log('approveRes', approveRes);
+                let instance_id = await contractIds[name].call();
 
-    //@dev - Step #8: Listing & Discovery
-    let purchaseRes = await converter.convert(smartToken.address, connectorToken.address, 500, 1);
+                let res = await contractRegistry.registerAddress(instance_id, instance.address);
 
-    console.log('purchaseRes', purchaseRes);
+            };
 
-    let purchaseAmount1 = getConversionAmount(purchaseRes);
+            let contracts = {};
 
-    console.log('purchase amount 1', purchaseAmount1)
+            let success = false;
+            let failures = 0;
+            while (!success && failures < 3) {
+                current = 0;
+                try {
 
-    let purchaseRes2 = await converter.convert(smartToken.address, connectorToken.address, 700, 1);
 
-    console.log('purchaseRes2', purchaseRes2);
+                    await trystage( deployer.deploy(ContractRegistry).then((instance) => {
+                        contracts['CONTRACT_REGISTRY'] = instance;
+                    }));
+                    let contractRegistry = contracts['CONTRACT_REGISTRY'];
 
-    let purchaseRes3 = await converter.convert(connectorToken.address, smartToken.address, 200, 1);
+                    await trystage( deployer.deploy(ContractIds).then((instance) => {
+                        contracts['CONTRACT_IDS'] = instance;
+                    }));
+                    let contractIds = contracts['CONTRACT_IDS'];
 
-    console.log('purchaseRes3', purchaseRes3);
+                    await trystage( deployer.deploy(ContractFeatures).then(async (instance) =>
+                        registerContract(instance, 'CONTRACT_FEATURES')
+                    ));
+
+                    await trystage( deployer.deploy(BancorGasPriceLimit, gasPrice).then(async (instance) =>
+                        registerContract(instance, 'BANCOR_GAS_PRICE_LIMIT')
+                    ));
+
+                    await trystage( deployer.deploy(BancorFormula).then(async (instance) =>
+                        registerContract(instance, 'BANCOR_FORMULA')
+                    ));
+
+                    // await trystage( deployer.deploy(NonStandardTokenRegistry).then(async (instance) =>
+                    //     registerContract(instance, 'NON_STANDARD_TOKEN_REGISTRY')
+                    // ));
+
+                    await trystage( deployer.deploy(BancorNetwork, contractRegistry.address)
+                        .then(async (instance) =>
+                            registerContract(instance, 'BANCOR_NETWORK')
+                        )
+                        // .then(async () =>{
+                        //     let res = await contracts['BANCOR_NETWORK'].setSignerAddress(accounts[0])
+                        // })
+                        );
+
+                    await trystage( deployer.deploy(BancorConverterFactory).then(async (instance) =>
+                        registerContract(instance, 'BANCOR_CONVERTER_FACTORY')
+                    ));
+
+                    await trystage( deployer.deploy(BancorConverterUpgrader, contractRegistry.address)
+                        .then(async (instance) =>
+                            registerContract(instance, 'BANCOR_CONVERTER_UPGRADER')
+                        ));
+
+                    let bancorXId = await contractIds.BANCOR_X.call();
+                    await contractRegistry.registerAddress(bancorXId, accounts[0]);
+
+
+                    //@dev - Step #2: Pool Token Deployment
+                    await trystage( deployer.deploy(SmartToken, 'SMART20', 'SM20', 2)
+                        .then(async (instance) =>
+                        {
+                            contracts['SMART_TOKEN'] = instance;
+                        }));
+
+                    let smartToken = contracts['SMART_TOKEN'];
+
+
+                    await trystage( deployer.deploy(TestERC20Token, 'Wrapped DAI 2', 'WDAI', 10000000)
+                    //await trystage( deployer.deploy(WrappedDai, 'Wrapped DAI 2', 'WDAI', 10000000)
+                        .then(async (instance) =>
+                        {
+                            contracts['CONNECTOR_1'] = instance;
+                        }));
+
+                    let connectorToken = contracts['CONNECTOR_1'];
+
+                    let weiAmount = Web3Utils.toWei('0.1');
+                    //let sendres = await connectorToken.sendTransaction({from: accounts[0], value: weiAmount});
+
+                    //@dev - Step #3: Converter Deployment
+                    await trystage( deployer.deploy(
+                        BancorConverter,
+                        smartToken.address, contractRegistry.address, 30000, contracts['CONNECTOR_1'].address, 250000
+                        ).then(async (instance) => {
+                            contracts['CONVERTER'] = instance;
+                        }));
+
+                    let converter = contracts['CONVERTER'];
+
+                    //@dev - Step #6: Funding & Initial Supply
+                    let ir = await smartToken.issue(accounts[0], 20000);
+                    // console.log('ir res', ir);
+
+                    //@dev - Step #7: Activation
+                    let tr = await contracts['CONNECTOR_1'].transfer(converter.address, 5000);
+                    // console.log('tr res', tr);
+
+                    //@dev - Step #7: Activation
+                    let t0r = await smartToken.transferOwnership(converter.address);
+                    // console.log('t0r res', t0r);
+
+                    //@dev - Step #7: Activation
+                    let a0r = await converter.acceptTokenOwnership();
+                    // console.log('a0r res', a0r);
+
+                    let approveRes = await connectorToken.approve(converter.address, 500000000);
+
+                    console.log('approveRes', approveRes);
+
+                    //@dev - Step #8: Listing & Discovery
+                    let purchaseRes = await converter.convert(smartToken.address, connectorToken.address, 500, 1);
+
+                    console.log('purchaseRes', purchaseRes);
+
+                    let purchaseAmount1 = getConversionAmount(purchaseRes);
+
+                    console.log('purchase amount 1', purchaseAmount1)
+
+                    let purchaseRes2 = await converter.convert(smartToken.address, connectorToken.address, 700, 1);
+
+                    console.log('purchaseRes2', purchaseRes2);
+
+                    let purchaseRes3 = await converter.convert(connectorToken.address, smartToken.address, 200, 1);
+
+                    console.log('purchaseRes3', purchaseRes3);
+
+                    success = true
+                } catch (e) {
+                    if (!retry) {
+                        throw e
+                    }
+                    failures += 1
+
+                    console.log('failure number: ', failures)
+                    console.log(e)
+                }
+
+            }
+
+        })
+    }
 
 };
